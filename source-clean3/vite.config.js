593ab6f27c23068593ec3254c70ffe0a8a27e73e
kluzi <killian.luzi@hotmail.fr>
2026-04-29 20:17:22 +0200

    Update vite.config.js

--- source-clean3/vite.config.js
+++ source-clean3/vite.config.js
@@ -1,26 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Planificateur de repas',
-        short_name: 'Repas',
-        description: 'Planifiez vos repas en famille',
-        theme_color: '#0D9E82',
-        background_color: '#F2F2F7',
-        display: 'standalone',
-        orientation: 'portrait',
-        icons: [
-          { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' }
-        ]
-      }
-    })
-  ]
+  plugins: [react()]
 })
